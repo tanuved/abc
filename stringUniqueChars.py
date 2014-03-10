2f46c8aabd720dedce3f53c4ac8140e592c9f499
Tanay <tanuved@gmail.com>
2014-03-10 18:58:46 -0400

    stringUniqueChars.py

---
+++ stringUniqueChars.py
@@ -0,0 +1,21 @@
+'''
+Problem: Determine if a string has all unique chars
+
+Tanay Ved
+2014
+'''
+
+def addAndTestStringWithDictionary(stringToTest):
+    dictionary = {}
+    for char in stringToTest:
+        cint = ord(char)
+        if char in d.values():
+            print False
+            break
+        d[cint] = char
+    print True
+
+def main():
+    userString = raw_input ("Enter a string to test: ")
+    addAndTestStringWithDictionary(userString)
+main()
